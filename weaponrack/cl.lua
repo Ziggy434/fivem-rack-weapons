ef3a6f4c4aadeb040c7310584a1211146f51c7d1
Ziggy <68012775+Ziggy434@users.noreply.github.com>
2022-01-03 21:13:41 +0800

    Added a wait time to racking and also sound fx

--- weaponrack/cl.lua
+++ weaponrack/cl.lua
@@ -13,6 +13,7 @@ config.arAmmoCount = 9999
 
 -- Constant Var's
 ped = GetPlayerPed(-1)
+netID = NetworkGetNetworkIdFromEntity(PlayerPedId(PlayerId()))
 arEquiped = false
 shotgunEquiped = false
 
@@ -23,15 +24,19 @@ RegisterCommand('ar', function()
     local isPoliceVeh = tostring(GetVehicleClass(currentVeh))
     if isPoliceVeh == "18" then
         if not arEquiped then
+            TriggerServerEvent("Server:SoundToRadius", netID, 3, "unrack", 0.8)
+            Citizen.Wait(600)
             GiveWeaponToPed(ped, GetHashKey(config.arName), config.arAmmoCount, false, false)
             GiveWeaponComponentToPed(ped, GetHashKey(config.arName), GetHashKey("COMPONENT_AT_AR_FLSH"))
             GiveWeaponComponentToPed(ped, GetHashKey(config.arName), GetHashKey("COMPONENT_AT_AR_AFGRIP"))
             arEquiped = true
-            notify("~b~Weapons: ~c~Rifle unracked from your cruiser!")
+            notify("~b~Weapons: ~c~Rifle ~C~unracked~c~ from your cruiser!")
         else
+            TriggerServerEvent("Server:SoundToRadius", netID, 3, "unrack", 0.8)
+            Citizen.Wait(600)
             RemoveWeaponFromPed(ped, GetHashKey(config.arName))
             arEquiped = false
-            notify("~b~Weapons: ~c~Rifle has been racked in your cruiser!")
+            notify("~b~Weapons: ~c~Rifle has been ~C~racked~c~ in your cruiser!")
         end
     else
         notify("~b~Weapons: ~r~You need to be in your patrol vehicle to unrack your rifle!")
@@ -45,20 +50,23 @@ RegisterCommand('shotgun', function()
     local isPoliceVeh = tostring(GetVehicleClass(currentVeh))
     if isPoliceVeh == "18" then
         if not shotgunEquiped then
+            TriggerServerEvent("Server:SoundToRadius", netID, 3, "unrack", 0.8)
+            Citizen.Wait(600)
             GiveWeaponToPed(ped, GetHashKey(config.shotgunName), config.shotgunAmmoCount, false, false)
             GiveWeaponComponentToPed(ped, GetHashKey(config.shotgunName), GetHashKey("COMPONENT_AT_AR_FLSH"))
             shotgunEquiped = true
-            notify("~b~Weapons: ~c~Shotgun unracked from your cruiser!")
+            notify("~b~Weapons: ~c~Shotgun ~C~unracked~c~ from your cruiser!")
         else
+            TriggerServerEvent("Server:SoundToRadius", netID, 3, "unrack", 0.8)
+            Citizen.Wait(600)
             RemoveWeaponFromPed(ped, GetHashKey(config.shotgunName))
             shotgunEquiped = false
-            notify("~b~Weapons: ~c~Shotgun has been racked in your cruiser!")
+            notify("~b~Weapons: ~c~Shotgun has been ~C~racked~c~ in your cruiser!")
         end
     else
         notify("~b~Weapons: ~r~You need to be in your patrol vehicle to unrack your shotgun!")
     end
 end)
-
 
 -- functions
 function notify(text)
